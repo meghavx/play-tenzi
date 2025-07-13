91ff31d4b0d1574305447349a4c961e2c8846e29
Megha Verma <mverma1427@gmail.com>
2025-07-13 23:06:33 +0530

    Replace Provider with ChakraProvider

--- src/components/ui/provider.jsx
+++
@@ -1,9 +0,0 @@
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
-
-export function Provider({ children }) {
-  return (
-    <ChakraProvider value={defaultSystem}>
-      {children}
-    </ChakraProvider>
-  )
-}
